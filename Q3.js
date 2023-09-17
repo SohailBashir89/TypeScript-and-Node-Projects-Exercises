6fce62ef0f0c4009baba11d36cc55b5fb6df6b7c
SohailBashir89 <sohailbashir89@gmail.com>
2023-09-17 07:55:33 +0500

    update

---
+++ Q3.js
@@ -0,0 +1,8 @@
+"use strict";
+/*Famous Quote: Find a quote from a famous person you admire.
+Print the quote and the name of its author. Your output should look something like the following,
+including the quotation marks:
+Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+Quid e Azam once said, “Expect the best, prepare for the worst.”*/
+var quotation = (`A Roman emperor Marcus Aurelius once said,"The opinion of 10,000 men is of no value if none of them know anything about the subject."`);
+console.log(quotation);
---
+++ Q4.js
@@ -0,0 +1,6 @@
+"use strict";
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
+//Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = ("A Roman emperor Marcus Aurelius");
+var messagee = (famous_person + ` once said,"The opinion of 10,000 men is of no value if none of them know anything about the subject."`);
+console.log(messagee);
